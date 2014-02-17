7fee30949d76110e926e0ec21aadc9a13cce1d97
guillermoarturovillagomez <guillermoarturovillagomez@gmail.com>
2014-02-16 23:11:34 -0600

    Usuarios
    Funciona registro, login y logout

--- ProyectoIndicadores/django/indicadores/home/views.py
+++ ProyectoIndicadores/django/indicadores/home/views.py
@@ -1,5 +1,71 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User as AuthUser
+from django.http import HttpResponseRedirect
+from django.db import IntegrityError
 
-# Create your views here.
 def login(request):
-	return render(request, 'home/login.html', {})
+	if request.method == 'POST':
+		usuario 	= request.POST['usuario']
+		password 	= request.POST['password']
+		user 		= authenticate(username=usuario, password=password)
+		if user is not None:
+			if user.is_active:
+				auth_login(request, user)
+				return HttpResponseRedirect('/dashboard/')
+			else:
+				return render(request, 'home/usuario/not_active.html')
+	elif request.user.is_authenticated():
+		if request.user.is_active:
+			return HttpResponseRedirect('/dashboard/')
+		else:
+			return render(request, 'home/usuario/not_active.html')
+	else:
+		return render(request, 'home/usuario/login.html')
+
+def logout(request):
+	auth_logout(request)
+	return HttpResponseRedirect('/login/')
+
+def registro_usuario(request):
+	mssg = []
+	if request.method == 'POST':
+		nombre 	 		  = request.POST['nombre']
+		apellido 		  = request.POST['apellido']
+		usuario  		  = request.POST['username']
+		password 		  = request.POST['password']		
+		try:
+			user 			  = AuthUser.objects.create_user(usuario, email=usuario + "@udec.edu.mx", password=password)
+			user.first_name   = nombre
+			user.last_name 	  = apellido
+			user.is_active 	  = False
+			user.is_staff	  = False
+			user.save()
+			return HttpResponseRedirect('/login/')
+		except IntegrityError:
+			mssg.append("Usuario Duplicado")	
+	else:
+		if request.user.is_authenticated():
+			if request.user.is_active:
+				return HttpResponseRedirect('/login/')
+			else:
+				return render(request, 'home/usuario/not_active.html')
+	return render(request, 'home/usuario/registro_usuario.html', { "errores":mssg })
+
+#############################################
+
+def dashboard(request):
+	if request.user.is_authenticated():
+		if request.user.is_active:
+			return render(request, 'home/base.html')
+	else:
+		return HttpResponseRedirect('/login/')
+
+#############################################
+
+def nueva_area(request):
+	return render(request, 'home/area/nuevo.html')
+def editar_area(request):
+	return render(request, 'home/area/nuevo.html')
+def borrar_area(request):
+	return render(request, 'home/area/nuevo.html')	
--- ProyectoIndicadores/django/indicadores/indicadores/urls.py
+++ ProyectoIndicadores/django/indicadores/indicadores/urls.py
@@ -11,6 +11,19 @@ urlpatterns = patterns('',
     # url(r'^$', 'indicadores.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', views.login, name="login"),
+    url(r'^admin/'		, include(admin.site.urls)),
+
+    url(r'^login/$'		, views.login, name="login"),
+    url(r'^logout/$'	, views.logout, name="logout"),
+    url(r'^registro/$'	, views.registro_usuario, name="registro_usuario"),
+    
+    url(r'^dashboard/'	, views.dashboard, name="dashboard"),
+    url(r'^$', views.dashboard, name="home"),
+
+    url(r'^area/nueva/' , views.nueva_area, name="nueva_area"),
+    url(r'^area/(?P<pk_area>[0-9]+)/editar/$', views.editar_area, name="editar_area"),    
+    url(r'^area/(?P<pk_area>[0-9]+)/borrar/$', views.borrar_area, name="borrar_area"),
+
+
+
 ) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
